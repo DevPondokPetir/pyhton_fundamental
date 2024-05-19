ef3053474a8615dbd75db6b11f0af617a3b2bb34
zakhiyah <azakhiyah@gmail.com>
2024-05-19 20:33:00 +0700

    rename variable in english

--- dasar_sintaksis_perulangan_baca_buku_dengan_while_undefined_count.py
+++ dasar_sintaksis_perulangan_baca_buku_dengan_while_undefined_count.py
@@ -2,29 +2,29 @@
 program perulangan membaca buku dengan while undefined count
 """
 
-jumlah_buku=10
-total_jumlah_baca=0
+book_count=10
+read_count=0
 print('Ibu berkata, "baca semua bukumu"')
 
-jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
-print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+understood_count= 0
+print(f'Jumlah buku yang sudah dibaca dan dipahami {understood_count}')
 
-while total_jumlah_baca < jumlah_buku:
-    total_jumlah_baca = total_jumlah_baca + 1
-    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
-        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1} belum paham")
+while read_count < book_count:
+    read_count = read_count + 1
+    if understood_count == 9:
+        print(f"Buku ke {understood_count + 1} belum paham")
     else:
-        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
-        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami")
+        understood_count = understood_count + 1
+        print(f"Buku ke {understood_count} sudah dibaca dan dipahami")
 
     #jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
     #print(f"Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
-print(f"Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
-if jumlah_buku_yang_sudah_dibaca_dan_dipahami == jumlah_buku:
+print(f"Jumlah buku yang sudah dibaca dan dipahami {understood_count}")
+if understood_count == book_count:
     print('"Bu, Semua buku sudah dibaca dan dipahami')
 else:
     print(f'"Bu,tidak semua buku bisa dipahami. '
-          f'Budi hanya bisa memahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku')
+          f'Budi hanya bisa memahami {understood_count} buku')
 
 
 
