e1c46b8fb994ddfe873275e22e34eba4b7834834
zakhiyah <azakhiyah@gmail.com>
2024-05-19 19:30:45 +0700

    initial commit

---
+++ dasar_sintaksis_joke_programer.py
@@ -0,0 +1,27 @@
+"""
+Semua sintaksi dalam bahasa pemrogramnan terdiri dari :
+1. Sequential : langkah berurutan
+2. Percabangan : langkah melompat jika kondisi terpenuhi
+3. Perulangan : mengulang langkah yang sama berkali- kali selama / sampai kondisi terpenuhi
+"""
+# 1.Sequential
+print ('Ibu berkata,"Pergi ke toko"')
+print ('Nabil menjawab,"Baik, apa yang harus saya lakukan di toko ?"')
+print ('Ibu menjawab,"Beli satu botol susu, dan jika ada telor beli 6"')
+print ('Maka Budi berangkat ke toko')
+print ('Dan mulai berbelanja')
+
+# 2.Percabangan
+
+milk_bottle_count = 173
+
+if milk_bottle_count > 0:
+    print("Budi mengecek harganya apakah cukup untuk membeli 1 botol susu, dan ternyata uangnya cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+
+
+
+
